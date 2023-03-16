60de26a86e72843402fef1985e57b238de4d79b4
Jovica <jovicar93@gmail.com>
2023-03-16 01:47:51 +0100

    Adding test bash scrips and screenshots of executed commands for  chapters: 13, 14, 15 and 16. of Linux bible.

---
+++ week-3/chapter-15/test7.sh
@@ -0,0 +1,4 @@
+ls: cannot access 'test': No such file or directory
+ls: cannot access 'badtest': No such file or directory
+-rw-rw-r-- 1 joco joco 208 Mar 15 21:48 test2
+-rw-rw-r-- 1 joco joco   0 Mar 15 21:49 test3
